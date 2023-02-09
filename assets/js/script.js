aba273fe9add0ed21ea8bdfd4e66e29da3e30541
ZuzanaElt <zuzanaeltom@gmail.com>
2023-02-09 10:17:28 +0000

    adding ninja api and fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,50 @@
 const giphyApi = "6AOXnBTIbFMl4rE7kd6emFGfdEfEDgUz"
-var queryURL = "https://api.giphy.com/v1/stickers/search?api_key=CsFg6rIT9VQThklrGrafYGaGHa378omF&q=elephant&limit=10&offset=0&rating=pg&lang=en";
+const ninjaApi = "nu0nGP8mTDfJcW2JSl2Fwg==VZ4ntEbwyUNsM6bO"
+var giphyStickersURL = "https://api.giphy.com/v1/stickers/search?api_key=CsFg6rIT9VQThklrGrafYGaGHa378omF&q=elephant&limit=10&offset=0&rating=pg&lang=en";
+
 let animals = [
     {animalName:'elephant',
     ApiID: ''},
-    {animalName:'sheep',
-    ApiID: ''},
-    {animalName:'giraff',
-    ApiID: ''},
-    {animalName:'whale',
-    ApiID: ''},
-    {animalName:'narval',
-    ApiID: ''},
-    {animalName:'rabbit',
-    ApiID: ''},
-    {animalName:'leopard',
-    ApiID: ''},
-    {animalName:'polar bear',
-    ApiID: ''},
-    {animalName:'wolf',
-    ApiID: ''},
     {animalName:'duck',
     ApiID: ''},
+    {animalName:'lion',
+    ApiID: ''},
+    {animalName:'shark',
+    ApiID: ''},
+    {animalName:'giraffe',
+    ApiID: ''},
+    {animalName:'eagle',
+    ApiID: ''},
+    {animalName:'octopus',
+    ApiID: ''},
+    {animalName:'bee',
+    ApiID: ''},
+    {animalName:'cow',
+    ApiID: ''},
+    {animalName:'hamste',
+    ApiID: ''},
+    {animalName:'owl',
+    ApiID: ''},
 ]
+let ninjaUrl = 'https://api.api-ninjas.com/v1/animals?name=pig'
 
-fetch(queryURL)
+// giphy data
+fetch(giphyStickersURL)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+
+  //fetching ninja data
+  let options = {
+    method: 'GET',
+    headers: { 'x-api-key': 'nu0nGP8mTDfJcW2JSl2Fwg==VZ4ntEbwyUNsM6bO' }
+  }
+  fetch(ninjaUrl,options)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data)
+        })
+        .catch(err => {
+          console.log(`error ${err}`)
+      }); 
+
+ 
